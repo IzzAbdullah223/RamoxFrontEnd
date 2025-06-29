56cd64a4a9dd1e191476aa0547c88336155b2c3b
Izz Abdallah <x3zoabdullah@gmail.com>
2025-06-29 09:03:33 +0400

    2nd commit

--- vite-project/src/Pages/Markets/Markets.tsx
+++ vite-project/src/Pages/Markets/Markets.tsx
@@ -1,9 +1,15 @@
 import MCSS from './Markets.module.css'
 import { GoLinkExternal } from "react-icons/go";
 import { BsPeopleFill } from "react-icons/bs";
-import { FiBarChart2 } from "react-icons/fi";
+import { LuRows2 } from "react-icons/lu";
 import { BsFillLightningChargeFill } from "react-icons/bs";
 import { HiOutlineChartBarSquare } from "react-icons/hi2";
+import { IoSearch } from "react-icons/io5";
+import { HiMiniBarsArrowDown } from "react-icons/hi2";
+import { FaStar } from "react-icons/fa";
+import { GoTriangleDown } from "react-icons/go";
+import { FaRegStar } from "react-icons/fa";
+import { FaRegFileAlt } from "react-icons/fa";
 <HiOutlineChartBarSquare/>
 
 function Markets(){
@@ -127,6 +133,391 @@ function Markets(){
 
                     </div>
                 </div>
+
+                <div className={MCSS.Top5}>
+                    <div className={MCSS.TopName}>
+                        <div className={MCSS.Icon}><BsPeopleFill></BsPeopleFill></div>
+                        <h2>Top 5 Soical Activity</h2>
+                    </div>
+
+                    <div className={MCSS.TopHeaders}>
+                        <div>TOKEN</div>
+                        <div>INTERACTIONS</div>
+                    </div>
+
+                    <div className={MCSS.TopSides}>
+                        <div className={MCSS.TopLeft}>
+
+                            <div className={MCSS.Token}>
+                                <img src='coin.png'></img>
+                                <div>
+                                    <div>Kava</div>
+                                    <div>KAVA</div>
+                                </div>
+                            </div>
+
+                            <div className={MCSS.Token}>
+                                <img src='coin.png'></img>
+                                <div>
+                                    <div>Kava</div>
+                                    <div>KAVA</div>
+                                </div>
+                            </div>
+
+                            <div className={MCSS.Token}>
+                                <img src='coin.png'></img>
+                                <div>
+                                    <div>Kava</div>
+                                    <div>KAVA</div>
+                                </div>
+                            </div>
+
+                            <div className={MCSS.Token}>
+                                <img src='coin.png'></img>
+                                <div>
+                                    <div>Kava</div>
+                                    <div>KAVA</div>
+                                </div>
+                            </div>
+
+                            <div className={MCSS.Token}>
+                                <img src='coin.png'></img>
+                                <div>
+                                    <div>Kava</div>
+                                    <div>KAVA</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={MCSS.TopRight}>
+
+                        <div className={MCSS.Interact}>
+                            <div>64</div>
+                            <div className={MCSS.SevenContainer}>
+                                <div className={MCSS.Seven}>7d</div>
+                                <div className={MCSS.Inc}>+255.56%</div>
+                            </div>
+                        </div>
+
+                        <div className={MCSS.Interact}>
+                            <div>64</div>
+                            <div className={MCSS.SevenContainer}>
+                                <div className={MCSS.Seven}>7d</div>
+                                <div className={MCSS.Inc}>+255.56%</div>
+                            </div>
+                        </div>
+
+                        <div className={MCSS.Interact}>
+                            <div>64</div>
+                            <div className={MCSS.SevenContainer}>
+                                <div className={MCSS.Seven}>7d</div>
+                                <div className={MCSS.Inc}>+255.56%</div>
+                            </div>
+                        </div>
+
+                        <div className={MCSS.Interact}>
+                            <div>64</div>
+                            <div className={MCSS.SevenContainer}>
+                                <div className={MCSS.Seven}>7d</div>
+                                <div className={MCSS.Inc}>+255.56%</div>
+                            </div>
+                        </div>
+
+                        <div className={MCSS.Interact}>
+                            <div>64</div>
+                            <div className={MCSS.SevenContainer}>
+                                <div className={MCSS.Seven}>7d</div>
+                                <div className={MCSS.Inc}>+255.56%</div>
+                            </div>
+                        </div>
+
+                 
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className={MCSS.Top5}>
+                    <div className={MCSS.TopName}>
+                        <div className={MCSS.Icon}><BsFillLightningChargeFill></BsFillLightningChargeFill></div>
+                        <h2>Top 5 Gainers</h2>
+                    </div>
+
+                    <div className={MCSS.TopHeaders}>
+                        <div>EXCHANGE</div>
+                        <div>TOTAL GAIN</div>
+                    </div>
+
+                    <div className={MCSS.TopSides}>
+                        <div className={MCSS.TopLeft}>
+
+                            <div className={MCSS.Token}>
+                                <img src='coin.png'></img>
+                                <div>
+                                    <div>Kava</div>
+                                    <div>KAVA</div>
+                                </div>
+                            </div>
+
+                            <div className={MCSS.Token}>
+                                <img src='coin.png'></img>
+                                <div>
+                                    <div>Kava</div>
+                                    <div>KAVA</div>
+                                </div>
+                            </div>
+
+                            <div className={MCSS.Token}>
+                                <img src='coin.png'></img>
+                                <div>
+                                    <div>Kava</div>
+                                    <div>KAVA</div>
+                                </div>
+                            </div>
+
+                            <div className={MCSS.Token}>
+                                <img src='coin.png'></img>
+                                <div>
+                                    <div>Kava</div>
+                                    <div>KAVA</div>
+                                </div>
+                            </div>
+
+                            <div className={MCSS.Token}>
+                                <img src='coin.png'></img>
+                                <div>
+                                    <div>Kava</div>
+                                    <div>KAVA</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={MCSS.TopRight}>
+
+                        <div className={MCSS.Interact}>
+                            <div>64</div>
+                            <div className={MCSS.SevenContainer}>
+                                <div className={MCSS.Seven}>7d</div>
+                                <div className={MCSS.Inc}>+255.56%</div>
+                            </div>
+                        </div>
+
+                        <div className={MCSS.Interact}>
+                            <div>64</div>
+                            <div className={MCSS.SevenContainer}>
+                                <div className={MCSS.Seven}>7d</div>
+                                <div className={MCSS.Inc}>+255.56%</div>
+                            </div>
+                        </div>
+
+                        <div className={MCSS.Interact}>
+                            <div>64</div>
+                            <div className={MCSS.SevenContainer}>
+                                <div className={MCSS.Seven}>7d</div>
+                                <div className={MCSS.Inc}>+255.56%</div>
+                            </div>
+                        </div>
+
+                        <div className={MCSS.Interact}>
+                            <div>64</div>
+                            <div className={MCSS.SevenContainer}>
+                                <div className={MCSS.Seven}>7d</div>
+                                <div className={MCSS.Inc}>+255.56%</div>
+                            </div>
+                        </div>
+
+                        <div className={MCSS.Interact}>
+                            <div>64</div>
+                            <div className={MCSS.SevenContainer}>
+                                <div className={MCSS.Seven}>7d</div>
+                                <div className={MCSS.Inc}>+255.56%</div>
+                            </div>
+                        </div>
+
+                 
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className={MCSS.Top5}>
+                    <div className={MCSS.TopName}>
+                        <div className={MCSS.Icon}><BsPeopleFill></BsPeopleFill></div>
+                        <h2>Top 5 Losers  </h2>
+                    </div>
+
+                    <div className={MCSS.TopHeaders}>
+                        <div>TOKEN</div>
+                        <div>TOTAL LOSS</div>
+                    </div>
+
+                    <div className={MCSS.TopSides}>
+                        <div className={MCSS.TopLeft}>
+
+                            <div className={MCSS.Token}>
+                                <img src='coin.png'></img>
+                                <div>
+                                    <div>Kava</div>
+                                    <div>KAVA</div>
+                                </div>
+                            </div>
+
+                            <div className={MCSS.Token}>
+                                <img src='coin.png'></img>
+                                <div>
+                                    <div>Kava</div>
+                                    <div>KAVA</div>
+                                </div>
+                            </div>
+
+                            <div className={MCSS.Token}>
+                                <img src='coin.png'></img>
+                                <div>
+                                    <div>Kava</div>
+                                    <div>KAVA</div>
+                                </div>
+                            </div>
+
+                            <div className={MCSS.Token}>
+                                <img src='coin.png'></img>
+                                <div>
+                                    <div>Kava</div>
+                                    <div>KAVA</div>
+                                </div>
+                            </div>
+
+                            <div className={MCSS.Token}>
+                                <img src='coin.png'></img>
+                                <div>
+                                    <div>Kava</div>
+                                    <div>KAVA</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={MCSS.TopRight}>
+
+                        <div className={MCSS.Interact}>
+                            <div>64</div>
+                            <div className={MCSS.SevenContainer}>
+                                <div className={MCSS.Seven}>7d</div>
+                                <div className={MCSS.Inc}>+255.56%</div>
+                            </div>
+                        </div>
+
+                        <div className={MCSS.Interact}>
+                            <div>64</div>
+                            <div className={MCSS.SevenContainer}>
+                                <div className={MCSS.Seven}>7d</div>
+                                <div className={MCSS.Inc}>+255.56%</div>
+                            </div>
+                        </div>
+
+                        <div className={MCSS.Interact}>
+                            <div>64</div>
+                            <div className={MCSS.SevenContainer}>
+                                <div className={MCSS.Seven}>7d</div>
+                                <div className={MCSS.Inc}>+255.56%</div>
+                            </div>
+                        </div>
+
+                        <div className={MCSS.Interact}>
+                            <div>64</div>
+                            <div className={MCSS.SevenContainer}>
+                                <div className={MCSS.Seven}>7d</div>
+                                <div className={MCSS.Inc}>+255.56%</div>
+                            </div>
+                        </div>
+
+                        <div className={MCSS.Interact}>
+                            <div>64</div>
+                            <div className={MCSS.SevenContainer}>
+                                <div className={MCSS.Seven}>7d</div>
+                                <div className={MCSS.Inc}>+255.56%</div>
+                            </div>
+                        </div>
+
+                 
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+            <div className={MCSS.AllTokensContainer}>
+                <div className={MCSS.AllTokensTop}>
+
+                    <h2>All Tokens</h2>
+                    <div className={MCSS.AllTokensRight}> 
+                        <div className={MCSS.SearchContainer}>
+                             <input type='text' placeholder='Search by Token name or tag'></input>
+                         <IoSearch></IoSearch>
+                        </div>
+
+                        <div className={MCSS.ColumnContainer}>
+                            <div>Columns</div>
+                            <LuRows2></LuRows2>
+                        </div>
+
+                        <div className={MCSS.FilterContainer}>
+                            <div>Filters</div>
+                            <HiMiniBarsArrowDown></HiMiniBarsArrowDown>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={MCSS.AllTokensSecond}>
+                     <FaStar></FaStar>
+                     <div className={MCSS.SecondRowContainer}>
+                        <div>Token</div>
+                        <GoTriangleDown></GoTriangleDown>
+                     </div>
+    
+                     <div className={MCSS.SecondRowContainer}>
+                        <div>Price</div>
+                        <GoTriangleDown></GoTriangleDown>
+                     </div>
+
+                     <div className={MCSS.SecondRowContainer}>
+                        <div>Market Cap</div>
+                        <GoTriangleDown></GoTriangleDown>
+                     </div>
+
+                     <div className={MCSS.SecondRowContainer}>
+                        <div>Volume</div>
+                        <GoTriangleDown></GoTriangleDown>
+                     </div>
+
+                     <div className={MCSS.SecondRowContainer}>
+                        <div>Social Following</div>
+                        <GoTriangleDown></GoTriangleDown>
+                     </div>
+
+                     <div className={MCSS.SecondRowContainer}>
+                        <div>Social Interactions</div>
+                        <GoTriangleDown></GoTriangleDown>
+                     </div>
+
+                     <div className={MCSS.SecondRowContainer}>
+                        <div>Circulating supply</div>
+                        <GoTriangleDown></GoTriangleDown>
+                     </div>
+                     <div>24th Curve</div>
+                </div>
+
+                <div className={MCSS.AllTokensThird}>
+                    <FaRegStar></FaRegStar>
+                    <FaRegFileAlt></FaRegFileAlt>
+                    <div className={MCSS.Token}>
+                        <img src='coin.png'></img>
+                        <div>
+                            <div>Kava</div>
+                            <div>KAVA</div>
+                        </div>
+                    </div>                   
+                </div>
+            
             </div>
             
         </div>
